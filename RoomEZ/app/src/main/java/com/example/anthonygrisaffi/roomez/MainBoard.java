eed11c5d5be84deefc153497baf7fa7114896265
Anthony Grisaffi <agrisaffi@ucsd.edu>
2015-01-27 15:44:44 -0800

    The rest of the last commit.

--- RoomEZ/app/src/main/java/com/example/anthonygrisaffi/roomez/MainBoard.java
+++ RoomEZ/app/src/main/java/com/example/anthonygrisaffi/roomez/MainBoard.java
@@ -19,7 +19,7 @@ public class MainBoard extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main_board, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
