022f8cd98e27d05c78074f78a026371b25403541
Kevin Dang <danggkev@gmail.com>
2015-02-12 02:22:04 -0800

    Successfully got the backend for Sign-In/Login to work for the Login screen. Check it out! (Will add comments soon)

--- RoomEZ/app/src/main/java/com/example/anthonygrisaffi/roomez/WelcomeActivity.java
+++ RoomEZ/app/src/main/java/com/example/anthonygrisaffi/roomez/WelcomeActivity.java
@@ -2,6 +2,7 @@ package com.example.anthonygrisaffi.roomez;
 
 import android.app.Activity;
 import android.app.DialogFragment;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -10,19 +11,42 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import me.drakeet.materialdialog.MaterialDialog;
 
 
 public class WelcomeActivity extends Activity {
 
+    //Text field used for login username
+    private EditText loginUsername;
+
+    //Text field used for login password
+    private EditText loginPassword;
+
+    //Text field used for sign up username
+    private EditText signupUsername;
+
+    //Text field used for sign up password
+    private EditText signupPassword;
+
+    //Text field used for sign up password (Entering again)
+    private EditText signupPassword2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
         BootstrapButton logininButton = (BootstrapButton) findViewById(R.id.welcomelogin);
         BootstrapButton signupButton = (BootstrapButton) findViewById(R.id.welcomesignup);
         signupButton.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +56,7 @@ public class WelcomeActivity extends Activity {
         });
         logininButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
+
                 loginPopup();
 
             }
@@ -51,14 +76,12 @@ public class WelcomeActivity extends Activity {
         LayoutInflater factory = LayoutInflater.from(this);
         View myView = factory.inflate(R.layout.activity_login_pop_up, null);
         LinearLayout hello = new LinearLayout(this);
-        // hello.setOrientation(LinearLayout.VERTICAL);
-        // final EditText textBoth
-        // final EditText titleBox = new EditText(this);
-        // final EditText passwordBox = new EditText(this);
-        // titleBox.setHint("Enter your e-mail address");
-        // passwordBox.setHint("Enter your password");
+        signupUsername = (EditText) myView.findViewById(R.id.popupemail);
+        signupPassword = (EditText) myView.findViewById(R.id.popuppassword);
+        signupPassword2 = (EditText) myView.findViewById(R.id.popuppasswordagain);
         hello.addView(myView);
-        //hello.addView(passwordBox);
+
+
 
         final MaterialDialog mMaterialDialog = new MaterialDialog(this);
         mMaterialDialog.setTitle("MaterialDialog");
@@ -66,8 +89,7 @@ public class WelcomeActivity extends Activity {
         final MaterialDialog materialDialog = mMaterialDialog.setPositiveButton("SIGN UP", new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent b = new Intent(WelcomeActivity.this, MainBoard.class);
-                startActivity(b);
+                signup();
                 mMaterialDialog.dismiss();
 
             }
@@ -88,12 +110,8 @@ public class WelcomeActivity extends Activity {
         LayoutInflater factory = LayoutInflater.from(this);
         View myView = factory.inflate(R.layout.activity_signup_pop_up, null);
         LinearLayout hello = new LinearLayout(this);
-        // hello.setOrientation(LinearLayout.VERTICAL);
-        // final EditText textBoth
-        // final EditText titleBox = new EditText(this);
-        // final EditText passwordBox = new EditText(this);
-        // titleBox.setHint("Enter your e-mail address");
-        // passwordBox.setHint("Enter your password");
+        loginUsername = (EditText) myView.findViewById(R.id.popupemail2);
+        loginPassword = (EditText) myView.findViewById(R.id.popuppassword2);
         hello.addView(myView);
         //hello.addView(passwordBox);
 
@@ -103,7 +121,9 @@ public class WelcomeActivity extends Activity {
         final MaterialDialog materialDialog = mMaterialDialog.setPositiveButton("LOGIN", new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mMaterialDialog.dismiss();
+                //LoginActivity other = new LoginActivity();
+                login();
+
 
             }
         });
@@ -123,6 +143,8 @@ public class WelcomeActivity extends Activity {
 
 
 
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -137,4 +159,129 @@ public class WelcomeActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    private void signup() {
+
+        String username = signupUsername.getText().toString().trim();
+        String password = signupPassword.getText().toString().trim();
+        String passwordAgain = signupPassword2.getText().toString().trim();
+
+        // Validate the sign up data
+        boolean validationError = false;
+        StringBuilder validationErrorMessage = new StringBuilder(getString(R.string.error_intro));
+        if (username.length() == 0) {
+            validationError = true;
+            validationErrorMessage.append(getString(R.string.error_blank_email));
+        }
+        if (password.length() == 0) {
+            if (validationError) {
+                validationErrorMessage.append(getString(R.string.error_join));
+            }
+            validationError = true;
+            validationErrorMessage.append(getString(R.string.error_blank_password));
+        }
+        if (!password.equals(passwordAgain)) {
+            if (validationError) {
+                validationErrorMessage.append(getString(R.string.error_join));
+            }
+            validationError = true;
+            validationErrorMessage.append(getString(R.string.error_mismatched_passwords));
+        }
+        validationErrorMessage.append(getString(R.string.error_end));
+
+        // If there is a validation error, display the error
+        if (validationError) {
+            Toast.makeText(WelcomeActivity.this, validationErrorMessage.toString(), Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // Set up a progress dialog
+        final ProgressDialog dialog = new ProgressDialog(WelcomeActivity.this);
+        dialog.setMessage(getString(R.string.progress_signup));
+        dialog.show();
+
+        // Set up a new Parse user
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+
+        // Call the Parse signup method
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                dialog.dismiss();
+                if (e != null) {
+                    // Show the error message
+                    Toast.makeText(WelcomeActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                } else {
+                    // Start an intent for the dispatch activity
+                    Intent intent = new Intent(WelcomeActivity.this, DispatchActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+
+
+    private void login() {
+
+        String username = loginUsername.getText().toString().trim();
+        String password = loginPassword.getText().toString().trim();
+
+        // Validate the log in data
+        boolean validationError = false;
+        StringBuilder validationErrorMessage = new StringBuilder(getString(R.string.error_intro));
+        if (username.length() == 0) {
+            validationError = true;
+            validationErrorMessage.append(getString(R.string.error_blank_email));
+        }
+        if (password.length() == 0) {
+            if (validationError) {
+                validationErrorMessage.append(getString(R.string.error_join));
+            }
+            validationError = true;
+            validationErrorMessage.append(getString(R.string.error_blank_password));
+        }
+        validationErrorMessage.append(getString(R.string.error_end));
+
+        // If there is a validation error, display the error
+        if (validationError) {
+            Toast.makeText(WelcomeActivity.this, validationErrorMessage.toString(), Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // Set up a progress dialog
+        final ProgressDialog dialog = new ProgressDialog(WelcomeActivity.this);
+        dialog.setMessage(getString(R.string.progress_login));
+        dialog.show();
+        // Call the Parse login method
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                dialog.dismiss();
+                if (e != null) {
+                    // Show the error message
+                    Toast.makeText(WelcomeActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                } else {
+                    // Start an intent for the dispatch activity
+                    Intent intent = new Intent(WelcomeActivity.this, DispatchActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+
+
+
+
+
+
+
 }
