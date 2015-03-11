ab53835657b8e65dad13e69a4ec0a00ef47aa518
Kevin Dang <danggkev@gmail.com>
2015-03-10 19:45:45 -0700

    Chat functionality implemented.

---
+++ RoomEZ/app/src/main/java/com/example/anthonygrisaffi/roomez/ListUsersActivity.java
@@ -0,0 +1,7 @@
+package com.example.anthonygrisaffi.roomez;
+
+/**
+ * Created by KD on 3/7/15.
+ */
+public class ListUsersActivity {
+}
---
+++ RoomEZ/app/src/main/java/com/example/anthonygrisaffi/roomez/MessagingActivity.java
@@ -0,0 +1,7 @@
+package com.example.anthonygrisaffi.roomez;
+
+/**
+ * Created by KD on 3/9/15.
+ */
+public class MessagingActivity {
+}
